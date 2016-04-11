a49396b85ff7b55e40d41a4e3b6dca596532752d
Daniel Yab <djun44@gmail.com>
2016-04-11 17:42:34 -0300

    'Create a new project' feature complete.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Bundle.require(:default, Rails.env)
+# Bundler.require(:default, Rails.env)
 
 module Ticketee
   class Application < Rails::Application
